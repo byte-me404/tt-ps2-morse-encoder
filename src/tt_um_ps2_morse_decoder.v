b178328fb28f5678eba5d0e66c0beacb410cd58e
Daniel Baumgartner <db.baumgartner@gmx.at>
2023-12-21 11:43:26 +0100

    TEST

--- src/tt_um_ps2_morse_decoder.v
+++ src/tt_um_ps2_morse_decoder.v
@@ -1,5 +1,6 @@
 `default_nettype none
 
+
 module tt_um_ps2_morse_decoder #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
     output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
@@ -11,50 +12,34 @@ module tt_um_ps2_morse_decoder #( parameter MAX_COUNT = 24'd10_000_000 ) (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-    wire reset = ! rst_n;
-    wire [6:0] led_out;
-    assign uo_out[6:0] = led_out;
-    assign uo_out[7] = 1'b0;
+    wire reset =! rst_n;
+	wire [7:0] test_received_data;
+    wire       test_received_data_strb;
+    
+    
+    // DUT
+    ps2_controller ps2_controller_DUT (
+        // Inputs
+        .clk(clk),
+        .rst(reset),
+        
+        // Bidirectionals
+        .ps2_clk(uio_in[0]),
+        .ps2_data(uio_in[0]),
+        
+        // Outputs
+        .ps2_received_data(test_received_data),
+        .ps2_received_data_strb(test_received_data_strb)
+    );
 
-    // use bidirectionals as outputs
-    assign uio_oe = 8'b11111111;
-
-    // put bottom 8 bits of second counter out on the bidirectional gpio
-    assign uio_out = second_counter[7:0];
-
-    // external clock is 10MHz, so need 24 bit counter
-    reg [23:0] second_counter;
-    reg [3:0] digit;
-
-    // if external inputs are set then use that as compare count
-    // otherwise use the hard coded MAX_COUNT
-    wire [23:0] compare = ui_in == 0 ? MAX_COUNT: {6'b0, ui_in[7:0], 10'b0};
-
-    always @(posedge clk) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == compare) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
+    data_control #(BUFFER_LENGTH) data_control_DUT (
+        // Inputs
+        .clk(clk),
+        .rst(reset),
+        .ps2_received_data(test_received_data),
+        .ps2_received_data_strb(test_received_data_strb),
+        
+        // Outputs
+        .morse_code_out(uo_out[0])
+    );
 endmodule
